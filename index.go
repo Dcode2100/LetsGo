de1c5837bd93c321ed1f7cd07952d475d25a2000
Dikshant Solanki <dikshantsolanki214@gmail.com>
2025-01-04 12:22:36 +0000

    Adds Interface and Struct implemenation

--- index.go
+++ index.go
@@ -2,7 +2,42 @@ package main
 
 import "fmt"
 
+type gasEngine struct{
+    mpg uint8
+    gallons uint8
+}
+
+type electricEngine struct{
+    mpkwh uint8
+    kwh uint8
+}
+
+func (e gasEngine) milesLeft() uint8 {
+    return e.gallons*e.mpg
+}
+
+func (e electricEngine) milesLeft() uint8 {
+    return e.kwh*e.mpkwh
+}
+
+type engine interface{
+    milesLeft() uint8
+}
+
+func canMakeIt(e engine, miles uint8){
+    if miles<=e.milesLeft(){
+        fmt.Println("You can make it there!")
+    }else{
+        fmt.Println("Need to fuel up first!")
+    }
+}
+
 func main() {
-    message := "Hello, Delve!"
-    fmt.Println(message)
+    // Create some example engines
+    gas := gasEngine{mpg: 30, gallons: 10}
+    electric := electricEngine{mpkwh: 4, kwh: 100}
+    
+    // Test both engines
+    canMakeIt(gas, 250)      // Test gas engine
+    canMakeIt(electric, 255) // Test electric engine
 }
